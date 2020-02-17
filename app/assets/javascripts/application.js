5876a8d3b58c08fac14280354b9f7e7b881d8503
Gabe Olivares <gabeolivares60@gmail.com>
2020-02-16 21:55:29 -0600

    Update long lat.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,8 @@
 
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: 29.533763, lng: -98.5640156};
+
+  var uluru = {lat: 29.5340962, lng: -98.5618342};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 15, center: uluru});
